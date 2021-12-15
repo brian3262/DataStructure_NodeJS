54513b1e69020fc1c83579b912c71865dbedda56
unknown <kfhwang@gm.nutc.edu.tw>
2021-12-15 14:00:07 +0800

    20211215

---
+++ Search.js
@@ -0,0 +1,5 @@
+var ary=[0,3,5,6,9,7,2,1]
+console.log(ary.indexOf(4));
+
+var data=[{name:"amy", age:20}, {name:"bob", age:15}, {name:"cubs", age:30}, {name:"dell", age:26}, {name:"eleven", age:11}]
+data.indexOf(15)
